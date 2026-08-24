d3511d6a1632c52d8b96d3dd7e3c166e8a114db6
María Fernanda Rubio Romero <20235795@esen.edu.sv>
2026-08-24 13:11:47 -0600

    ci(backend): configura workflow de lint y tests (#1)

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+﻿const js = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'eqeqeq': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+  prettier,
+];
---
+++ tests/unit/index.test.js
@@ -0,0 +1,5 @@
+﻿const { ping } = require('../../src/index');
+
+test('ping devuelve pong', () => {
+  expect(ping()).toBe('pong');
+});
